84d1b632aeef1bdb142d1bbda26be94e59c9cf95
Sakiyama Shingo <yamaa.exception@gmail.com>
2025-04-20 20:43:48 +0900

    react-hook-formで書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { FormEvent } from "react";
+import { useUserForm, UserFormInputs } from "./hooks/useUserForm";
 
 export default function Home() {
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.target as HTMLFormElement);
-    const name = formData.get("name");
-    const gender = formData.get("gender");
-    console.log(name, gender);
+  const { registerName, registerGender, handleSubmit, errors } = useUserForm();
+
+  const onSubmit = (data: UserFormInputs) => {
+    console.log(data);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="name" />
-      <input type="radio" name="gender" value="male" />男
-      <input type="radio" name="gender" value="female" />女<button>送信</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...registerName} />
+      {errors.name && <p>{errors.name.message}</p>}
+      <input type="radio" {...registerGender} value="male" />男
+      <input type="radio" {...registerGender} value="female" />女
+      <button>送信</button>
+      {errors.gender && <p>{errors.gender.message}</p>}
     </form>
   );
 }
---
+++ src/hooks/useUserForm.ts
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+
+export type UserFormInputs = {
+  name: string;
+  gender: string;
+};
+
+export const useUserForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormInputs>({ mode: "onChange" });
+
+  const registerName = register("name", {
+    required: "名前は必須です",
+    maxLength: { value: 10, message: "10文字以内で入力してください" },
+  });
+
+  const registerGender = register("gender");
+
+  return {
+    errors,
+    registerName,
+    registerGender,
+    handleSubmit: handleSubmit,
+  };
+}; 
